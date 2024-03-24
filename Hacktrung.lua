f33d930e8dc1427ee1167394bcd1496c29e872bc
vuquang111234 <156691896+vuquang111234@users.noreply.github.com>
2024-03-24 19:07:54 +0700

    Update and rename README.md to Hacktrung.lua

---
+++ Hacktrung.lua
@@ -0,0 +1,29 @@
+{
+    "result": {
+        "totalPages": 1,
+        "scripts": [
+            {
+                "_id": "61f075deac292897751ba311",
+                "title": "Get Event Items for free",
+                "game": {
+                    "gameId": 7541759836,
+                    "name": "\u51f9\u51f8\u4e16\u754c\uff1a\u81ea\u7531\u4e1b\u6797",
+                    "imageUrl": "https://tr.rbxcdn.com/34625905f895c245048edff107476615/500/280/Image/Jpeg"
+                },
+                "slug": "Get-Event-Items-for-free_468",
+                "verified": false,
+                "views": 6839,
+                "scriptType": "free",
+                "isUniversal": false,
+                "isPatched": false,
+                "createdAt": "2022-01-25T22:12:46.841Z",
+                "updatedAt": "2024-03-24T11:15:05.445Z",
+                "__v": 0,
+                "script": "loadstring(game:HttpGet(\"https://scriptblox.com/raw/Get-Event-Items-for-free_468\"))()",
+                "matched": [
+                    "game"
+                ]
+            }
+        ]
+    }
+}
